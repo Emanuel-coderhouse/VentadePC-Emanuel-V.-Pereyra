995fd0f9a93098696e3833de2526561aec7694f6
Emanuel-coderhouse <78391179+Emanuel-coderhouse@users.noreply.github.com>
2021-06-15 20:18:29 -0300

    Add files via upload

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import './Header.css';
+
+class Header extends Component{
+    render () {
+        return (
+            <nav className= 'navigation-bar'>
+                <h1>HeaderTest</h1>
+                
+            </nav>
+            
+        )
+    }
+}
+
+export default Header;
